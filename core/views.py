4050991fa157a2ee820f1ea83b81158fc2aa913a
Kereece <kereece.williams@quinnipiac.edu>
2016-04-05 21:59:45 +0000

    Add team delete view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
@@ -50,3 +50,8 @@ class TeamUpdateView(UpdateView):
     model = Team
     template_name = 'team/team_form.html'
     fields = ['name', 'description']
+    
+class TeamDeleteView(DeleteView):
+    model = Team
+    template_name = 'team/team_confirm_delete.html'
+    success_url = reverse_lazy('team_list')
